ab0aa1b22c67ac2319afd15c3a394b3ecc79ad16
Davit Egoian <dato.egoyan@gmail.com>
2025-11-04 23:42:17 +0400

    Constants have been added

---
+++ src/main/java/ge/tbc/APITestAutomation/data/Constants.java
@@ -0,0 +1,7 @@
+package data;
+
+public class Constants {
+    public static final String BASE_URL = "https://apigw.tbcbank.ge/api/v1";
+    public static final String EXCHANGE_RATE_ENDPOINT = "/exchangeRates/getExchangeRate?Iso1=USD&Iso2=GEL";
+    public static final String COMMERCIAL_RATES_ENDPOINT = "/commercialRates/list";
+}
