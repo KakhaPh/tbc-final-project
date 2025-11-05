98023a35c059174a6bdd4b0bb4fdec6decad5808
Mariam Ubiria <tutt.ubiria@gmail.com>
2025-11-05 12:18:49 +0400

    Added ConstantsAPI

---
+++ src/main/java/ge/tbc/testautomation/api/data/ConstantsAPI.java
@@ -0,0 +1,12 @@
+package ge.tbc.testautomation.api.data;
+
+public class ConstantsAPI {
+
+    public static final String BASE_URL_EXCHANGE ="https://apigw.tbcbank.ge/api/v1";
+    public static final String DEFAULT_EXCHANGE_PAGE_URL="https://tbcbank.ge/ka/treasury-products?amount=100&ccyFrom=USD&ccyTo=GEL";
+    public static final String ORIGIN_URL="https://tbcbank.ge";
+    public static final String BASE_PATH_EXCHANGE_RATE ="/exchangeRates";
+    public static final String COMMERCIAL_LIST_ENDPOINT ="/commercialRates/list";
+    public static final String EURO_RATE_ENDPOINT ="/getExchangeRate?Iso1=EUR&Iso2=GEL";
+
+}
