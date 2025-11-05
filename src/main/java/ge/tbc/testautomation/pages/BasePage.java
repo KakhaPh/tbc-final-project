1c1437443fc5eee2f3f3ef94d80a3d20ede4037b
Mariam Ubiria <tutt.ubiria@gmail.com>
2025-11-05 11:40:15 +0400

    Added BasePage and QuickLoanPage classes

--- src/main/java/ge/tbc/testautomation/pages/BasePage.java
+++ src/main/java/ge/tbc/testautomation/pages/BasePage.java
@@ -4,12 +4,24 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 
 public class BasePage {
+    public Page page;
     public final Locator headerPersonal;
     public final Locator burgerMenu;
 
     public BasePage(Page page) {
+        this.page = page;
         this.headerPersonal = page.locator("//div[text()=' ჩემთვის ' or text()=' Personal ']");
         this.burgerMenu = page.locator("//tbcx-icon[text()='burger-menu-alt-outlined']");
     }
+
+    public Locator navigationItem(String itemName) {
+        return page.locator("//div[contains(@class, 'tbcx-pw-navigation-item__link') and normalize-space(text()) = '" + itemName + "']");
+    }
+
+    public Locator megaMenuSubItem(String itemName) {
+        return  page.locator("//tbcx-pw-mega-menu-sub-item//span[text() = '" + itemName + "']");
+    }
+
+
 }
 
